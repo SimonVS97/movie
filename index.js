fbd04d8f4c813ea305f0f4cab7ed9976cea6844e
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-06-09 21:14:37 +0200

    added error handling

--- index.js
+++ index.js
@@ -67,6 +67,12 @@ app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
 
+// Error Handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 
 // listen for requests
 app.listen(8080, () => {
