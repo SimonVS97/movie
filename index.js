c2f559951d3518964cd74601057302b9702c4e8f
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-06-12 08:18:51 +0200

    added endpoints for requests

--- index.js
+++ index.js
@@ -63,9 +63,55 @@ app.get('/', (req, res) => {
   res.sendFile('/documentation.html', { root: __dirname });
 });*/
 
+// Get all movies
 app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
+
+// Get a single movie
+app.get('/movies/:title', (req, res) => {
+  let title = req.params.title;
+
+  res.json(topMovies.find((movie) => {
+    return movie.name === title;
+  }));
+
+});
+
+// Get data on a genre
+app.get('/genre/:name', (req,res) => {
+  res.send('Successful GET request returns data on a specific genre.');
+});
+
+// Get data about a director by name
+app.get('/directors/:name', (req,res) => {
+  res.send('Successful GET request returns data on a specific genre.');
+});
+
+// Allow new user to register
+app.put('/user/newuser', (req,res) => {
+  res.send('Successful PUT request adds a new user');
+});
+
+// Update user info
+app.post('/user/:name', (req,res) => {
+  res.send('Successful POST request updates info of user');
+});
+
+// Add a new movie to their list of favorites
+app.put('/user/:name/favoritelist', (req,res) => {
+  res.send('Successful PUT request adds a newe movie to list of favorite movies of user.')
+});
+
+// Deletes a movie from thier list of favorites
+app.delete('/user/:name/favoritelist/:moviename', (req,res) => {
+  res.send('Successfull DELETE request delets a movie from the list of favoirtes of a user.');
+});
+
+// Delets a user
+app.delete('/user/:name', (req,res) => {
+  res.send('Successful DELETE request deregisters a user.')
+})
 
 // Error Handling
 app.use((err, req, res, next) => {
